a49bb4431824b4b514fe6d52dcb584b2e7eb11ec
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2022-03-09 10:46:10 -0500

    [ADD] function to press space bar for shooting instead of holding the key

--- Game1007Project/Game1007Project/include/InputHandler.h
+++ Game1007Project/Game1007Project/include/InputHandler.h
@@ -9,5 +9,9 @@ public:
 	void Draw();
 	void Update();
 	bool quit = false;
+private:
 	SDL_Event event;
+	const Uint8* m_KeyboardStates = SDL_GetKeyboardState(NULL);
+	bool KEYS[];  // 322 is the number of SDLK_DOWN events
+
 };
--- Game1007Project/Game1007Project/source/InputHandler.cpp
+++ Game1007Project/Game1007Project/source/InputHandler.cpp
@@ -2,32 +2,45 @@
 #include "gameVector.h"
 #include "script.h"
 
+
 // Script component no need to draw.
 void InputHandler::Draw()
 {
 
 }
 
-
 // This metherd is called by engine each frame.
 void InputHandler::Update()
 {
+	for (int i = 0; i < 322; i++) 
+	{
+		KEYS[i] = false;
+	}
+
 	//Handle events on queue
 	while (SDL_PollEvent(&event) != 0)
-		{
+	{
 		//User requests quit
 		if(event.type == SDL_QUIT)
 		{
 			quit = true;
 		}
+
+		if (event.type == SDL_KEYDOWN)
+		{
+			KEYS[event.key.keysym.sym] = true;
+		}
+		else if (event.type == SDL_KEYUP)
+		{
+			KEYS[event.key.keysym.sym] = false;
+		}
 	}
-	const Uint8* m_KeyboardStates = SDL_GetKeyboardState(NULL);
 	if (m_KeyboardStates[SDL_SCANCODE_W])
 	{
 		Vector2* up = transform->position;
 		if (up->y > 0)
 		{
-			up->y -= 4;
+			up->y -= 5;
 			transform->position = up;
 		}
 	}
@@ -36,7 +49,7 @@ void InputHandler::Update()
 		Vector2* down = transform->position;
 		if (down->y < 405)
 		{
-			down->y += 4;
+			down->y += 5;
 			transform->position = down;
 		}
 	}
@@ -45,7 +58,7 @@ void InputHandler::Update()
 		Vector2* left = transform->position;
 		if (left->x > 0)
 		{
-			left->x -= 4;
+			left->x -= 5;
 			transform->position = left;
 		}
 	}
@@ -54,13 +67,13 @@ void InputHandler::Update()
 		Vector2* right = transform->position;
 		if (right->x < 710)
 		{
-			right->x += 4;
+			right->x += 5;
 			transform->position = right;
 		}
 	}
-
-	if (m_KeyboardStates[SDL_SCANCODE_SPACE]) 
+	if (KEYS[SDLK_SPACE])
 	{
+		cout << "hola\n";
 		Image* pImg;
 		GameObject* pGoMainShipBullet = new GameObject();
 		pImg = new Image("MainShipBullet.png", 3);
@@ -71,14 +84,18 @@ void InputHandler::Update()
 		bulletTransform->position->x = this->transform->position->x;
 		bulletTransform->position->y = this->transform->position->y;
 		bulletTransform->position->x += this->transform->size->x * this->transform->scale->x;
-		bulletTransform->position->y += bulletTransform->size->x * bulletTransform->scale->x;
+		bulletTransform->position->y += (bulletTransform->size->x * bulletTransform->scale->x);
 		bulletTransform->position->y -= (bulletTransform->size->y * bulletTransform->scale->y) / 2;
-		
-		ExampleProjectileScript *pProjectileScrip = new ExampleProjectileScript();
+
+		ExampleProjectileScript* pProjectileScrip = new ExampleProjectileScript();
 		Vector2 flyVector;
 		flyVector.x = 10;
 		flyVector.y = 0;
 		pProjectileScrip->SetFlyVector(flyVector);
 		pGoMainShipBullet->AddComponent<ExampleProjectileScript>(pProjectileScrip);
+	}
+	else
+	{
+		false;
 	}
 }
